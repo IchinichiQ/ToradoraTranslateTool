fd1437b6f8e565d86182e6c2be48b35a3571bdcf
12135555 <PechenkinPavelV@yandex.ru>
2020-09-03 23:26:19 +0300

    Move dlls to the bin folder

--- ToradoraTranslateTool/Program.cs
+++ ToradoraTranslateTool/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,9 +16,24 @@ namespace ToradoraTranslateTool
         [STAThread]
         static void Main()
         {
+            AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += MyResolve;
+            AppDomain.CurrentDomain.AssemblyResolve += MyResolve;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormMain());
+        }
+
+        private static Assembly MyResolve(Object sender, ResolveEventArgs e)
+        {
+            string assemblyPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Bin" ,new AssemblyName(e.Name).Name + ".dll");
+            if (File.Exists(assemblyPath))
+            {
+                Assembly assembly = Assembly.LoadFrom(assemblyPath);
+                return assembly;
+            }
+            else
+                return null;
         }
     }
 }
